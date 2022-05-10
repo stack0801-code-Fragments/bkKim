933743b47c33a9524b3958f7d929af2a64a5ba54
kimbg <rlaqudrhks6@gmail.com>
2022-05-10 21:47:02 +0900

    타겟넘버 dfs 추가

--- 타겟넘버.cpp
+++ 타겟넘버.cpp
@@ -33,3 +33,27 @@ int calc(vector<int>numbers,int target)
 int solution(vector<int> numbers, int target) {    
     return calc(numbers, target);
 }
+
+// 위가 BFS 아래가 DFS
+
+#include<algorithm>
+#include<vector>
+
+using namespace std;
+int cnt = 0;
+
+void DFS(vector<int>numbers, int target, int idx, int currentValue)
+{
+	if (idx == numbers.size()) {
+		if (currentValue == target)
+			cnt++;		
+		return;
+	}	
+	DFS(numbers, target, idx + 1, currentValue + numbers[idx]);
+	DFS(numbers, target, idx + 1, currentValue - numbers[idx]);
+}
+
+int solution(vector<int> numbers, int target) {
+	DFS(numbers, target, 0, 0);
+	return cnt;
+}
